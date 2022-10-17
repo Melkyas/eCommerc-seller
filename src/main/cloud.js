160bf141358ed43e895711df20448f9e8d629db8
Melkyas <82463687+Melkyas@users.noreply.github.com>
2022-10-17 11:25:54 -0400

    update

--- src/main/cloud.js
+++ src/main/cloud.js
@@ -1,20 +1,4 @@
-/*
- * --------------------------------------------------------------------------- *
- * File: cloud.js                                                              *
- * Project: seller                                                             *
- * Created Date: 16 Oct 2022                                                   *
- * Author: Vikas K Solegaonkar (vikas@crystalcloudsolutions.com)               *
- * Copyright (c) 2022 Vikas K Solegaonkar                                      *
- * Crystal Cloud Solutions (https://crystalcloudsolutions.com)                 *
- *                                                                             *
- * Last Modified: Sun Oct 16 2022                                              *
- * Modified By: Vikas K Solegaonkar                                            *
- *                                                                             *
- * HISTORY:                                                                    *
- * --------------------------------------------------------------------------- *
- * Date         By     Comments                                                *
- * --------------------------------------------------------------------------- *
- */
+
 
 import axios from "axios";
 
